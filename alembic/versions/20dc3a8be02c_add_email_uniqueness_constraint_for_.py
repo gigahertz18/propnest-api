a60b2a2a4ee6724ffd5ed9b7c4d05d6407762031
Adrianne De Guzman <adeedeguzman28@gmail.com>
2026-08-10 18:05:27 +0800

    ghi-38: Fix lint error.

--- alembic/versions/20dc3a8be02c_add_email_uniqueness_constraint_for_.py
+++ alembic/versions/20dc3a8be02c_add_email_uniqueness_constraint_for_.py
@@ -8,7 +8,6 @@ Create Date: 2026-08-10 09:51:39.156708
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '20dc3a8be02c'
